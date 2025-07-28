39730c5e9e3cdb29a37d03a754670394d2888930
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2025-07-28 16:02:09 +0900

    空のプロジェクトを作成

---
+++ README.md
@@ -0,0 +1,3 @@
+# petitcon24
+
+[第24回UE5ぷちコン](https://historia.co.jp/ue5petitcon24)用の Unreal Engine プロジェクトです。
---
+++ Source/petitcon24/petitcon24.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "petitcon24.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, petitcon24, "petitcon24" );
