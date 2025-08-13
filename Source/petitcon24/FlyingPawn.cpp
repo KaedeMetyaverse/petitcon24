51b1e58e52701d399c460a8cdde049edb75ec44f
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2025-08-13 15:13:46 +0900

    FlyingPawn に SkeletalMesh, Camera を追加

--- Source/petitcon24/FlyingPawn.cpp
+++ Source/petitcon24/FlyingPawn.cpp
@@ -1,6 +1,16 @@
 #include "FlyingPawn.h"
-#include "GameFramework/CharacterMovementComponent.h"
 
 AFlyingPawn::AFlyingPawn()
 {
+    SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+    RootComponent = SceneRoot;
+
+    SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+    SkeletalMesh->SetupAttachment(SceneRoot);
+    SkeletalMesh->SetRelativeRotation(FRotator(0, -90, 0));
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SceneRoot);
+    Camera->SetRelativeLocation(FVector(-360, 0, 20));
+    Camera->SetRelativeRotation(FRotator(-10, 0, 0));
 }
--- Source/petitcon24/FlyingPawn.h
+++ Source/petitcon24/FlyingPawn.h
@@ -2,6 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "UObject/ObjectPtr.h"
+#include "Components/SceneComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
 #include "FlyingPawn.generated.h"
 
 UCLASS()
@@ -11,6 +15,15 @@ class PETITCON24_API AFlyingPawn : public APawn
 
 public:
     AFlyingPawn();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+    TObjectPtr<USceneComponent> SceneRoot;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+    TObjectPtr<USkeletalMeshComponent> SkeletalMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+    TObjectPtr<UCameraComponent> Camera;
 };
 
 
